d7bfd50cd731dba18627cba7d2be0b3a9d5d2b73
Nayara Santos <naycsm226@gmail.com>
2023-10-02 15:19:15 -0300

    feat: new configs.

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,8 +1,11 @@
 import "express-async-errors";
 import "reflect-metadata";
 import express from "express";
+import middlewares from "./middlewares";
 
 const app = express();
 app.use(express.json());
+
+app.use(middlewares.handleErrors);
 
 export default app;
