5722c57dce4032a201b3a833a7dbef2509db3229
Nayara Santos <naycsm226@gmail.com>
2023-10-02 13:02:28 -0300

    feat: new configs.

---
+++ back/src/entities/index.ts
@@ -0,0 +1,7 @@
+import User from "./User.entity";
+import Address from "./Address.entity";
+import Comment from "./Comment.entity";
+import Anouncement from "./Anouncement.entity";
+import Image from "./Image.entity";
+
+export { User, Address, Comment, Anouncement, Image };
---
+++ back/src/repositories/index.ts
@@ -0,0 +1,13 @@
+import userRepository from "./user.repository";
+import addressRepository from "./address.repository";
+import commentRepository from "./comment.repository";
+import anouncementRepository from "./anouncement.repository";
+import imageRepository from "./image.repository";
+
+export {
+  userRepository,
+  addressRepository,
+  commentRepository,
+  anouncementRepository,
+  imageRepository,
+};
